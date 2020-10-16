5a29b0130a71e955efc3683df48470bc207bcb9a
Pierre Chiesa <pierre.chiesa@outlook.fr>
2020-10-16 11:39:49 +0200

    modifs partie 4

---
+++ spring-circuitdebreaker-service/src/main/java/com/ecommerce/microcommerce/circuitdebreakerservice/delegate/ServiceDelegate.java
@@ -0,0 +1,44 @@
+package com.ecommerce.microcommerce.circuitdebreakerservice.delegate;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+@Service
+public class ServiceDelegate {
+	@Autowired
+	RestTemplate restTemplate;
+	
+	@HystrixCommand(fallbackMethod = "callServiceAndGetData_Fallback")
+	public String callServiceAndGetData(int id) {
+		System.out.println("Getting details for " + id);
+		String response = restTemplate
+				.exchange("http://localhost:9090/Produits/{id}"
+				, HttpMethod.GET
+				, null
+				, new ParameterizedTypeReference<String>() {
+			}, id).getBody();
+
+		System.out.println("Response Received as " + response + " -  " + new Date());
+
+		return "NORMAL FLOW !!! - id -  " + id + " :::  Details " + response + " -  " + new Date();
+	}
+	
+	@SuppressWarnings("unused")
+	private String callServiceAndGetData_Fallback(int id) {
+		System.out.println("Service is down!!! fallback route enabled...");
+		return "CIRCUIT BREAKER ENABLED!!!No Response From Service at this moment. Service will be back shortly - " + new Date();
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+}
